79e9be4ca2f1dcaff574c7b5b69fd807836648bb
nabusim <cristianjose@gmail.com>
2024-05-31 23:22:49 +0200

    Tailwindcss configuracion y estructura de directorios

---
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Hola mundo fast</h1>
+  `,
+  styles: ``
+})
+export class HeavyLoadersFastComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loaders',
+  standalone: true,
+  imports: [],
+  template: `
+   <h1>Hola mundo users</h1>
+  `,
+  styles: ``
+})
+export class UsersLoadersComponent {
+
+}
